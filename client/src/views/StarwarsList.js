2c2312bf5e395b053142f36451a8bf7266454d75
Vincent1218 <vicentchai@hotmail.com>
2021-03-08 21:01:54 +0800

    update

--- client/src/views/StarwarsList.js
+++ client/src/views/StarwarsList.js
@@ -26,7 +26,7 @@ const StarwarsList = () => {
     var temp = data.length;
     var i;
     for (i = 0; i < temp; i++) {
-      if((data[i].count)>1){
+      if((data[i].count)>2){
         delete data[i]
       }
     }
