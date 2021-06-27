e6fbee761b9f50d54b6cbfd4c87a0912d208d7e3
ethan <hoolee2014@gmail.com>
2021-06-27 15:48:20 +0800

    Added MatchTwo.js

--- client/src/views/MatchTwo.js
+++ client/src/views/MatchTwo.js
@@ -14,59 +14,109 @@ const MatchTwo = () => {
             </div>
             
             <div className="match mainBlockBracket">
-              <p className="tableheader">循环赛</p>
+              <p className="tableheader">12进6淘汰赛</p>
               <table>
                 <tbody>
                 <tr>
-                <td rowspan="2">价值型</td>
-                <td rowspan="2">AB</td>
-                <td>正：科技是自由意志的牢笼</td>
+                <td rowspan="2" className="category">价值型</td>
+                <td rowspan="2" className="group">AB</td>
+                <td className="topic">正：科技是自由意志的牢笼</td>
                 </tr>
                 <tr>
-                <td>反：科技是自由意志的羽翼</td>
+                <td className="topic">反：科技是自由意志的羽翼</td>
                 </tr>
                 <tr>
-                <td rowspan="2">价值型</td>
-                <td rowspan="2">CD</td>
-                <td>正：低欲望社会的形成是社会进步的表现</td>
+                <td rowspan="2" className="category">价值型</td>
+                <td rowspan="2" className="group">CD</td>
+                <td className="topic">正：低欲望社会的形成是社会进步的表现</td>
                 </tr>
                 <tr>
-                <td>反：低欲望社会的形成是社会退步的表现</td>
+                <td className="topic">反：低欲望社会的形成是社会退步的表现</td>
                 </tr>
                 <tr>
-                <td rowspan="2">事实型</td>
-                <td rowspan="2">EF</td>
-                <td>正：应该以侵权为由遏止短视频二创的疯狂增长</td>
+                <td rowspan="2" className="category">事实型</td>
+                <td rowspan="2" className="group">EF</td>
+                <td className="topic">正：应该以侵权为由遏止短视频二创的疯狂增长</td>
                 </tr>
                 <tr>
-                <td>反：不应该以侵权为由遏止短视频二创的疯狂增长</td>
+                <td className="topic">反：不应该以侵权为由遏止短视频二创的疯狂增长</td>
                 </tr>
                 <tr>
-                <td rowspan="2">价值型</td>
-                <td rowspan="2">GH</td>
-                <td>正：当代文学作品在翻译时，应该支持译者的&ldquo;创造性叛逆&rdquo;</td>
+                <td rowspan="2" className="category">价值型</td>
+                <td rowspan="2" className="group">GH</td>
+                <td className="topic">正：当代文学作品在翻译时，应该支持译者的&ldquo;创造性叛逆&rdquo;</td>
                 </tr>
                 <tr>
-                <td>反：当代文学作品在翻译时，不应该支持译者的&ldquo;创造性叛逆&rdquo;</td>
+                <td className="topic">反：当代文学作品在翻译时，不应该支持译者的&ldquo;创造性叛逆&rdquo;</td>
                 </tr>
                 <tr>
-                <td rowspan="2">事实型</td>
-                <td rowspan="2">IJ</td>
-                <td>正：后疫情时代加速了全球化趋势</td>
+                <td rowspan="2" className="category">事实型</td>
+                <td rowspan="2" className="group">IJ</td>
+                <td className="topic">正：后疫情时代加速了全球化趋势</td>
                 </tr>
                 <tr>
-                <td>反：后疫情时代减缓了全球化趋势</td>
+                <td className="topic">反：后疫情时代减缓了全球化趋势</td>
                 </tr>
                 <tr>
-                <td rowspan="2">事实型</td>
-                <td rowspan="2">KL</td>
-                <td>正：选举后，民选议员跳槽应该接受法律制裁</td>
+                <td rowspan="2" className="category">事实型</td>
+                <td rowspan="2" className="group">KL</td>
+                <td className="topic">正：选举后，民选议员跳槽应该接受法律制裁</td>
                 </tr>
                 <tr>
-                <td>反：选举后，民选议员跳槽不应该接受法律制裁</td>
+                <td className="topic">反：选举后，民选议员跳槽不应该接受法律制裁</td>
                 </tr>
                 </tbody>
             </table>
+              <p className="tableheader">6进4排位赛</p>
+              <table>
+                <tbody>
+                <tr>
+                <td rowspan="2" className="serial">M</td>
+                <td className="topic">正：积极冲浪是后真相时代更好的生存之道</td>
+                </tr>
+                <tr>
+                <td className="topic">反：远离喧嚣是后真相时代更好的生存之道</td>
+                </tr>
+                <tr>
+                <td rowspan="2" className="serial">N</td>
+                <td className="topic">正：艺术作品评级对艺术文化发展利大于弊</td>
+                </tr>
+                <tr>
+                <td className="topic">反：艺术作品评级对艺术文化发展弊大于利</td>
+                </tr>
+                </tbody>
+              </table>
+              <p className="tableheader">半决赛</p>
+              <table>
+                <tbody>
+                <tr>
+                <td rowspan="2" className="serial">O1</td>
+                <td className="topic">正：无用之用方为大用</td>
+                </tr>
+                <tr>
+                <td className="topic">反：有用之用方为大用</td>
+                </tr>
+                <tr>
+                <td rowspan="2" className="serial">O2</td>
+                <td className="topic">正：喜剧贵在引人发笑</td>
+                </tr>
+                <tr>
+                <td className="topic">反：喜剧贵在引人思考</td>
+                </tr>
+                </tbody>
+              </table> 
+              <p className="tableheader">决赛</p>
+              <table>
+                <tbody>
+                <tr>
+                <td rowspan="2" className="serial">P1</td>
+                <td className="topic">正： 把人生看作一幅蓝图的人生态度更应该被追求</td>
+                </tr>
+                <tr>
+                <td className="topic">反： 把人生看作一场冒险的人生态度更应该被追求</td>
+                </tr>
+                </tbody>
+              </table>        
             </div>
         </body>        
       </div>
